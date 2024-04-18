0449bf65f10092c99341874d6100230d852bdfc5
Thomas Solecki <thomasjsolecki@gmail.com>
2024-04-17 19:33:15 -0700

    Add styling to TypeMatchupTable

--- src/components/TypeMatchupTable.tsx
+++ src/components/TypeMatchupTable.tsx
@@ -8,40 +8,42 @@ export default function TypeMatchupTable({
         return <></>;
     }
     return (
-        <table>
-            <tr>
-                <th>Super effective:</th>
-                <td className="flex">
-                    {type_table_data.super_effective.map((type_data) => (
-                        <TypeMatchupIcon
-                            type={type_data.type}
-                            multiplier={type_data.multiplier}
-                        />
-                    ))}
-                </td>
-            </tr>
-            <tr>
-                <th>Not very effective:</th>
-                <td className="flex">
-                    {type_table_data.not_very_effective.map((type_data) => (
-                        <TypeMatchupIcon
-                            type={type_data.type}
-                            multiplier={type_data.multiplier}
-                        />
-                    ))}
-                </td>
-            </tr>
-            <tr>
-                <th>Immune to:</th>
-                <td className="flex">
-                    {type_table_data.no_effect.map((type_data) => (
-                        <TypeMatchupIcon
-                            type={type_data.type}
-                            multiplier={type_data.multiplier}
-                        />
-                    ))}
-                </td>
-            </tr>
+        <table className="bg-green-400 rounded-md max-w-xl">
+            <tbody className="flex flex-col gap-2 p-2">
+                <tr className="bg-green-300 rounded-md p-2">
+                    <th className="text-left">Super effective:</th>
+                    <td className="flex p-1 gap-1 flex-wrap">
+                        {type_table_data.super_effective.map((type_data) => (
+                            <TypeMatchupIcon
+                                type={type_data.type}
+                                multiplier={type_data.multiplier}
+                            />
+                        ))}
+                    </td>
+                </tr>
+                <tr className="bg-green-300 rounded-md p-2">
+                    <th>Not very effective:</th>
+                    <td className="flex p-1 gap-1 flex-wrap">
+                        {type_table_data.not_very_effective.map((type_data) => (
+                            <TypeMatchupIcon
+                                type={type_data.type}
+                                multiplier={type_data.multiplier}
+                            />
+                        ))}
+                    </td>
+                </tr>
+                <tr className="bg-green-300 rounded-md p-2">
+                    <th>Immune to:</th>
+                    <td className="flex p-1 gap-1 flex-wrap">
+                        {type_table_data.no_effect.map((type_data) => (
+                            <TypeMatchupIcon
+                                type={type_data.type}
+                                multiplier={type_data.multiplier}
+                            />
+                        ))}
+                    </td>
+                </tr>
+            </tbody>
         </table>
     );
 }
